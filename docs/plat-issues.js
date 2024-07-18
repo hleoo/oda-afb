a7d465405c71140dc29aa71b5b3656cd8a245c4c
Leo <48129608+hleoo@users.noreply.github.com>
2024-07-18 18:06:00 +0700

    Update plat-issues.js

--- docs/plat-issues.js
+++ docs/plat-issues.js
@@ -1,10 +1,40 @@
+function extractTextBetweenKeywords(text, keywords) {
+    const results = {};
+    
+    for (let i = 0; i < keywords.length - 1; i++) {
+        const startKeyword = keywords[i];
+        const endKeyword = keywords[i + 1];
+        const regex = new RegExp(`${startKeyword}(.*?)${endKeyword}`, 's');
+        const match = text.match(regex);
+        
+        if (match && match[1]) {
+            const key = startKeyword.replace(':', '').trim();
+            const value = match[1].trim();
+            results[key] = value;
+        }
+    }
+
+    // Handle the last section
+    const lastKeyword = keywords[keywords.length - 1];
+    const lastRegex = new RegExp(`${lastKeyword}(.*)`, 's');
+    const lastMatch = text.match(lastRegex);
+
+    if (lastMatch && lastMatch[1]) {
+        const key = lastKeyword.replace(':', '').trim();
+        const value = lastMatch[1].trim();
+        results[key] = value;
+    }
+
+    return results;
+}
+
 window.sendIssue = function(senderID, webhookURL){
 
     var currentUrl = window.location.href;
     var issueID = document.querySelector('[data-test-id="issueDetails"]').textContent.substring(2);
     var issueTitle = document.querySelector('[data-test-id="issueTitle"]').value;
     var issueStatus = document.querySelector('[data-test-id="issueStatus"]').innerText.replace('Status\n','');    ;
-    var issueDesc = document.querySelector('[data-test-id="IssuePageIssueDescription"]').textContent;  
+    var issueDescRaw = document.querySelector('[data-test-id="IssuePageIssueDescription"]').textContent;  
     var issueReporter = document.querySelector('[data-test-id="issueCreatedBy"]').innerText.split('\n')[1];
     var issueSeverity = document.querySelector('[data-test-id="issueSeverity"]').innerText.split('\n')[1];  
     var issueIntegration = document.querySelector('[data-test-id="issueIntegrations"]').innerText.split('\n')[1];
@@ -14,11 +44,22 @@ window.sendIssue = function(senderID, webhookURL){
 
     issueLabels.pop(); // last item is empty
 
+    const keywords = [
+        'Environment:', 
+        'Test Case information:', 
+        'Steps to reproduce:', 
+        'Expected result:', 
+        'Actual result:'
+    ];
+
+    var issueDesc = extractTextBetweenKeywords(issueDescRaw, keywords);
+
     var data = {
         url: currentUrl,
         issueID: issueID,
         title: issueTitle,
         description: issueDesc,
+        rawDescription: issueDescRaw,
         status: issueStatus,
         reporter: issueReporter,
         severity: issueSeverity,
@@ -29,7 +70,7 @@ window.sendIssue = function(senderID, webhookURL){
         senderID: senderID
     };
 
-    console.table(data);
+    console.table(issueDesc);
 
     // Send data to webhook
     fetch(webhookURL, {
